c951924bd38b3b9340906b6d2e16ea51ec4cdfab
Mohit Thapliyal <mohitthapliyal100@gmail.com>
2023-03-05 18:09:14 +0530

    dummy data added

--- src/components/Json.jsx
+++ src/components/Json.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import dummyData from "../dummyData";
 
 const Json = ({ className, setJsonForm, setJsonFormHeading }) => {
   const [formHeading, setFormHeading] = useState("")
@@ -24,7 +25,7 @@ const Json = ({ className, setJsonForm, setJsonFormHeading }) => {
           setFormHeading(e.target.value);
         }}
           type="text"
-          className="w-full bg-gray-800 px-4 py-3 text-sm outline-none rounded-lg font-bold"
+          className="w-full bg-gray-800 px-4 py-3 text-sm outline-none rounded-lg"
           placeholder="Enter the Heading"
         />
       </div>
@@ -39,12 +40,16 @@ const Json = ({ className, setJsonForm, setJsonFormHeading }) => {
           placeholder="Enter the JSON"
         ></textarea>
       </div>
+      <div className="flex w-full gap-2 px-2">
+        <div onClick={()=>{setFormHeading("Yummy Pizza"); setFormData(JSON.stringify(dummyData[0]))}} className="flex flex-grow justify-center text-sm font-medium bg-violet-800 py-2 rounded-md active:scale-95 duration-200 cursor-pointer">Dummy Data 1</div>
+        <div onClick={()=>{setFormHeading("Tasty Pasta"); setFormData(JSON.stringify(dummyData[1]))}} className="flex flex-grow justify-center text-sm font-medium bg-violet-800 py-2 rounded-md active:scale-95 duration-200 cursor-pointer">Dummy Data 2</div>
+      </div>
       <div className="w-full p-2">
         <button
           onClick={submitHandler}
           type="submit"
           disabled={formData === "" || formHeading === ""}
-          className="bg-violet-800 py-2 uppercase w-full rounded-md active:scale-95 disabled:scale-100 disabled:opacity-50 disabled:cursor-not-allowed"
+          className="bg-violet-800 py-2 uppercase w-full rounded-md duration-200 active:scale-95 disabled:scale-100 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           submit
         </button>
